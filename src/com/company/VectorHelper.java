6eb1b4255eaa1f205c7082a164163e64a5ea1e41
oussama <fs_sabour@esi.dz>
2017-10-25 00:22:51 +0100

    ajouter mes fonctions

---
+++ src/com/company/VectorHelper.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by yesrefm on 22/10/2017.
+ */
+public class VectorHelper
+{
+/**
+     * verfier si deux vecteurr ont la meme taille
+     * @param vector1 le 1er vecteur
+     * @param vector2 le 2eme vecteur
+     * @throws VectorException l'exception qui declanch lorsque deux vecteurs ont la meme taille
+     */
+
+    private void verfVector(int [] vector1,int [] vector2 ) throws VectorException
+    {
+        if(vector1.length!=vector2.length) throw new VectorException();
+    }
+
+    /**
+     * la somme de deux vecterus
+     * @param vector1 le 1er vecteur
+     * @param vector2 le 2eme vecteur
+     * @return vecteur somme des deux vecteurs parametre
+     */
+    public int [] somme(int [] vector1,int [] vector2)
+    {
+
+        int [] somme=new int[vector1.length];
+        try {
+            verfVector(vector1,vector2);
+
+            for(int i=0;i<vector1.length;i++)
+            {
+                somme[i]=vector1[i]+vector2[i];
+            }
+        }
+        catch (VectorException e)
+        {
+            e.printStackTrace();
+        }
+        return somme;
+
+    }
+
+    /**
+     * inverser un vecteur
+     * @param vector le vecteur qu'on veut inverser
+     * @return le vecteur inverse de vecteur entrée
+     */
+    public int []  invers(int [] vector)
+    {
+        int [] result= new int[vector.length];
+        int j=0;
+        for(int i=vector.length-1;i>=0;i--)
+        {
+            result[j]=vector[i];
+            j++;
+        }
+        return result;
+    }
