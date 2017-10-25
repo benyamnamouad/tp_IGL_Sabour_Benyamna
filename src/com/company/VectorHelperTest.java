4acc8932ca32095ab4c9e7e44759e9e1370b57f6
sseoussama <30413494+sseoussama@users.noreply.github.com>
2017-10-25 01:04:34 +0100

    modifier par moi

--- src/com/company/VectorHelperTest.java
+++ src/com/company/VectorHelperTest.java
@@ -8,7 +8,14 @@ import org.junit.Test;
  */
 public class VectorHelperTest extends VectorHelper
 {
-    
+     @Test
+    public void testInversse()
+    {
+        Assert.assertArrayEquals(new int []{6,5,4,3,2,1},invers(new int []{1,2,3,4,5,6}));
+    }
+    @Test
+    public void testSomme() {
+        Assert.assertArrayEquals(new int []{2,4,6,8,10,12},somme(new int []{1,2,3,4,5,6},new int []{1,2,3,4,5,6}));}  
     @Test
     public void testTrier() {
         Assert.assertArrayEquals(new int []{1,2,3,4,5,6},trier(new int []{3,2,1,4,6,5}));
