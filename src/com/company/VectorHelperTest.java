848e8565850960613ac0ed26c0e9536dabe44a30
BnMouad <dev_bnmouad@hotmail.com>
2017-10-25 01:06:41 +0100

    documentation de la class Test

--- src/com/company/VectorHelperTest.java
+++ src/com/company/VectorHelperTest.java
@@ -8,23 +8,41 @@ import org.junit.Test;
  */
 public class VectorHelperTest extends VectorHelper
 {
+	 /**
+     * Test de la focntion inverse
+     */
      @Test
     public void testInversse()
     {
         Assert.assertArrayEquals(new int []{6,5,4,3,2,1},invers(new int []{1,2,3,4,5,6}));
     }
+	 /**
+     * Test de la fonction Somme
+     */
     @Test
     public void testSomme() {
         Assert.assertArrayEquals(new int []{2,4,6,8,10,12},somme(new int []{1,2,3,4,5,6},new int []{1,2,3,4,5,6}));}  
+		
+		 /**
+     * Test de la fonction Trier
+     */
+	 
     @Test
     public void testTrier() {
         Assert.assertArrayEquals(new int []{1,2,3,4,5,6},trier(new int []{3,2,1,4,6,5}));
     }
+	
+	  /**
+     * Test de la fonction max_min
+     */
     @Test
-    public void testMaxMib()
+    public void testMaxMin()
     {
         Assert.assertArrayEquals(new int[]{1, 34}, min_max(new int[]{3, 2, 1, 4, 6, 34}));
     }
+	/**
+     * Test de la fonction multiplier
+     */
     @Test
     public void testMultiplier()//test multiplication
     {
